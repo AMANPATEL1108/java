aff627b37a588e3d68813a0600467a01c220fab3
AMAN PATEL <amanpatel8844382@gmail.com>
2024-02-21 18:09:26 +0530

    Practice Day4

---
+++ practice/Wrapingclass.java
@@ -0,0 +1,7 @@
+public class Wrapingclass {
+    public static void main(String[] args) {
+        Integer myInt = 100;
+        String myString = myInt.toString();
+        System.out.println(myString.length());
+    }
+}
