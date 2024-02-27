50973b81201caaea9201ff94f88111b03df22a6a
AMAN PATEL <amanpatel8844382@gmail.com>
2024-02-27 07:43:26 +0530

    Practice Day10

---
+++ Questions/average.java
@@ -0,0 +1,19 @@
+package Questions;
+
+/**
+ * average
+ */
+public class average {
+
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 7;
+        int num3 = 9;
+        int num4 = 6;
+        int num5 = 4;
+
+        int total = num1 + num2 + num3 + num4 + num5;
+        int Average = total / 5;
+        System.out.println("everage is :" + Average);
+    }
+}
