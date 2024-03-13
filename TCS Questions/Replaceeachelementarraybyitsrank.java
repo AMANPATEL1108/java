17c714ec4fd148307c8a828bbd3a592cc47cc419
AMAN PATEL <amanpatel8844382@gmail.com>
2024-03-13 07:17:57 +0530

    Practice Day25

---
+++ TCS Questions/Replaceeachelementarraybyitsrank.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Replaceeachelementarraybyitsrank {
+    public static void main(String args[]) {
+        int n = 6;
+        int arr[] = { 20, 15, 26, 2, 98, 6 };
+        for (int i = 0; i < n; i++) {
+            Set<Integer> s = new HashSet<Integer>();
+            for (int j = 0; j < n; j++) {
+                if (arr[j] < arr[i]) {
+                    s.add(arr[j]);
+                }
+            }
+            int rank = s.size() + 1;
+            System.out.print(rank + " ");
+        }
+    }
+}
