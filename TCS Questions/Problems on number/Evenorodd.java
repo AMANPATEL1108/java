e14a9f3057aadd5bbafa0f56fbbead7a670f8a1d
AMAN PATEL <amanpatel8844382@gmail.com>
2024-03-27 07:08:51 +0530

    Practice Day39

---
+++ TCS Questions/Problems on number/Evenorodd.java
@@ -0,0 +1,11 @@
+public class Evenorodd {
+    public static void main(String[] args) {
+        int n = 100;
+        if (n % 2 == 0) {
+            System.out.println("number is Even");
+
+        } else {
+            System.out.println("number is odd");
+        }
+    }
+}
