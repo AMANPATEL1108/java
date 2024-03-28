b1439d941ba33735a49e1e18c192a032c7a95ee7
AMAN PATEL <amanpatel8844382@gmail.com>
2024-03-28 07:18:50 +0530

    Practice Day40

---
+++ TCS Questions/Problems on number/Checkweathergivennumberispositiveornegative.java
@@ -0,0 +1,10 @@
+public class Checkweathergivennumberispositiveornegative {
+    public static void main(String[] args) {
+        int n = -12;
+        if (n < 0) {
+            System.out.println(n + " Number is nagatibve");
+        } else {
+            System.out.println(n + " Number is positive");
+        }
+    }
+}
