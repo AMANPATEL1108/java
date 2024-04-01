7e1fb79c921d837a0d1ff4640ae5b20303a99e74
AMAN PATEL <amanpatel8844382@gmail.com>
2024-04-01 07:00:16 +0530

    Practice Day44

---
+++ TCS Questions/Problems on number/Greatestoftwonumber.java
@@ -0,0 +1,9 @@
+public class Greatestoftwonumber {
+    public static void main(String[] args) {
+        int num1 = 45;
+        int num2 = 55;
+
+        System.out.println("Greatest Number is :" + Math.max(num1, num2));
+
+    }
+}
