b113c4a14f60109f1c882e6390be6f432b021f31
AMANPATEL <amanpatel8844382@gmail.com>
2024-04-23 06:58:41 +0530

    Practice Day66

---
+++ TCS Questions/Problems on number/Calculateareaofcircle.java
@@ -0,0 +1,11 @@
+public class Calculateareaofcircle {
+    public static void areaofCircle(int n) {
+        double ans = 3.14 * n * n;
+        System.out.println("Area of Circle is :" + ans);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        areaofCircle(n);
+    }
+}
