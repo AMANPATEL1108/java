abfe77e86cf1e565e075dde088370d58c6ab5850
AMANPATEL <amanpatel8844382@gmail.com>
2024-04-28 07:50:15 +0530

    Practice Day71

---
+++ TCS Questions/Problem on Number System/Convertdecimaltooctal.java
@@ -0,0 +1,20 @@
+public class Convertdecimaltooctal {
+
+    public static int Decimaltooctal(int Decimal) {
+        int i = 0;
+        int octal = 0;
+        while (Decimal != 0) {
+            int rem = Decimal % 8;
+            octal += rem * Math.pow(10, i);
+            i++;
+            Decimal /= 8;
+        }
+        return octal;
+    }
+
+    public static void main(String[] args) {
+        int Decimal = 136;
+        System.out.println("The Octal conversion of the given decimal number is " + Decimaltooctal(Decimal));
+    }
+
+}
