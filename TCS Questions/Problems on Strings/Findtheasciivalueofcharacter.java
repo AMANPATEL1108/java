194a6b4b955662f2e621ed909117bc93679aecf1
AMANPATEL <amanpatel8844382@gmail.com>
2024-05-09 06:23:28 +0530

    Practice Day82

---
+++ TCS Questions/Problems on Strings/Findtheasciivalueofcharacter.java
@@ -0,0 +1,11 @@
+public class Findtheasciivalueofcharacter {
+    public static void value() {
+        char c = 'e';
+        int ascii = c;
+        System.out.println("The AscII value of" + c + " is " + ascii);
+    }
+
+    public static void main(String[] args) {
+        value();
+    }
+}
