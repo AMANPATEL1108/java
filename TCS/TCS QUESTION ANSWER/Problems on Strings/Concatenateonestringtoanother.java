81c3fdd2f3d3af5f8c7d58c3d339e160bdda3519
AMANPATEL <amanpatel8844382@gmail.com>
2024-06-01 06:56:16 +0530

    Practice Day105

---
+++ TCS/TCS QUESTION ANSWER/Problems on Strings/Concatenateonestringtoanother.java
@@ -0,0 +1,10 @@
+public class Concatenateonestringtoanother {
+    public static void main(String[] args) {
+        String str1 = "aman";
+        String str2 = "patel";
+
+        str1 = str1 + str2;
+
+        System.out.println(str1);
+    }
+}
