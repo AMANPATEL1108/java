a6ec6d0c2c53777a750bbfba72ef504db167926b
AMANPATEL <amanpatel8844382@gmail.com>
2024-06-05 06:45:31 +0530

    32 W3resorces Completed

---
+++ Course/w3resourceExercise/JavaBasic/BasicExcercisePart1/Writeaprogramtocomparetwonumbers.java
@@ -0,0 +1,39 @@
+package Course.w3resourceExercise.JavaBasic.BasicExcercisePart1;
+
+/**
+ * Writeaprogramtocomparetwonumbers
+ */
+public class Writeaprogramtocomparetwonumbers {
+
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 39;
+
+        if (a == b) {
+            System.out.println(a + "==" + b);
+
+        }
+
+        if (a != b) {
+            System.out.println(a + "!=" + b);
+
+        }
+        if (a < b) {
+            System.out.println(a + "<" + b);
+
+        }
+        if (a > b) {
+            System.out.println(a + ">" + b);
+
+        }
+        if (a <= b) {
+            System.out.println(a + "<=" + b);
+
+        }
+        if (a >= b) {
+            System.out.println(a + ">=" + b);
+
+        }
+
+    }
+}
