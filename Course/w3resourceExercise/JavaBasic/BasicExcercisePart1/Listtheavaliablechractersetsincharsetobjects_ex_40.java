3b9265bb51afbd4f6310821b0fa7ee67b238d089
AMANPATEL <amanpatel8844382@gmail.com>
2024-06-11 05:54:36 +0530

    W3resources ex 41,42 completed

--- Course/w3resourceExercise/JavaBasic/BasicExcercisePart1/Listtheavaliablechractersetsincharsetobjects_ex_40.java
+++ Course/w3resourceExercise/JavaBasic/BasicExcercisePart1/Listtheavaliablechractersetsincharsetobjects_ex_40.java
@@ -3,7 +3,7 @@ import java.nio.charset.Charset;
 public class Listtheavaliablechractersetsincharsetobjects_ex_40 {
     public static void main(String[] args) {
         System.out.println("List of the avaliable character sets:");
-
+w3wwqwe,rhw
         for (String str : Charset.availableCharsets().keySet()) {
             System.out.println(str);
         }
---
+++ Course/w3resourceExercise/JavaBasic/BasicExcercisePart1/Printtheasciivalueofthegivencharacter_ex_41.java
@@ -0,0 +1,7 @@
+public class Printtheasciivalueofthegivencharacter_ex_41 {
+    public static void main(String[] args) {
+        int chr = 'Z';
+
+        System.out.println("The ASCII value of Z is:" + chr);
+    }
+}
