8a6f52664c55e29e5c20d537ad3b720f2598f6b5
AMANPATEL <amanpatel8844382@gmail.com>
2024-06-12 06:51:07 +0530

    w3resources 43,44 completed

---
+++ Course/w3resourceExercise/JavaBasic/BasicExcercisePart1/Acceptsanintergerandcomputesthevalueofn_nn_nnn_ex_44.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Acceptsanintergerandcomputesthevalueofn_nn_nnn_ex_44 {
+    public static void main(String[] args) {
+
+        int n;
+        char s1, s2, s3;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input String:");
+        n = in.nextInt();
+
+        System.out.printf("%d+%d%d+%d%d%d\n", n, n, n, n, n, n);
+    }
+}
