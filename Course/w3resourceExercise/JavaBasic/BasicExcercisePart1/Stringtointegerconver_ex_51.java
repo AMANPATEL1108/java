89dffe4905b3ace02ae97e7159ee49b6ae6d8911
AMANPATEL <amanpatel8844382@gmail.com>
2024-06-16 06:35:49 +0530

    W3Resources EX-51,52 Completed

---
+++ Course/w3resourceExercise/JavaBasic/BasicExcercisePart1/Stringtointegerconver_ex_51.java
@@ -0,0 +1,9 @@
+public class Stringtointegerconver_ex_51 {
+    public static void main(String[] args) {
+        String str = "123";
+        String str2 = "456";
+
+        System.out.println(Integer.parseInt(str) + Integer.parseInt(str2));
+
+    }
+}
---
+++ Course/w3resourceExercise/JavaBasic/BasicExcercisePart1/Sumtwonumberfirstandseconequaltothirdthenreturntrue_ex_52.java
@@ -0,0 +1,14 @@
+public class Sumtwonumberfirstandseconequaltothirdthenreturntrue_ex_52 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = 15;
+
+        int d = a + b;
+        if (d == c) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
