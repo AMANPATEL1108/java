c67eae6c77e5589eacf22dce05bb0451b2bcc02e
AMANPATEL <amanpatel8844382@gmail.com>
2024-07-04 08:28:24 +0530

    39 Graph Completed

---
+++ Course/Apna College/37_39_Graph_Part_1_3/GraphProblem.java
@@ -0,0 +1,214 @@
+import java.util.*;
+
+public class GraphProblem {
+
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
+
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 1));
+
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 4, 2));
+
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 4, 1));
+        graph[3].add(new Edge(3, 5, 1));
+
+        graph[4].add(new Edge(4, 2, 2)); // Corrected the weight
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 5, 1));
+
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 6, 1));
+
+        graph[6].add(new Edge(6, 5, 1)); // Added reverse edge for consistency
+    }
+
+    public static void bfs(ArrayList<Edge>[] graph) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] vis = new boolean[graph.length];
+        q.add(0); // Start BFS from vertex 0
+
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+
+            if (!vis[curr]) {
+                System.out.print(curr + " ");
+                vis[curr] = true;
+                for (int i = 0; i < graph[curr].size(); i++) {
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static void dfs(ArrayList<Edge>[] graph, int curr, boolean[] vis) {
+        System.out.print(curr + " ");
+        vis[curr] = true;
+
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (!vis[e.dest]) {
+                dfs(graph, e.dest, vis);
+            }
+        }
+    }
+
+    public static boolean hasPath(ArrayList<Edge>[] graph, int src, int dest, boolean[] vis) {
+        if (src == dest) {
+            return true;
+        }
+        vis[src] = true;
+
+        for (int i = 0; i < graph[src].size(); i++) {
+            Edge e = graph[src].get(i);
+            if (!vis[e.dest] && hasPath(graph, e.dest, dest, vis)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void calcIndeg(ArrayList<Edge>[] graph, int[] indeg) {
+        for (int i = 0; i < graph.length; i++) {
+            for (int j = 0; j < graph[i].size(); j++) {
+                Edge e = graph[i].get(j);
+                indeg[e.dest]++;
+            }
+        }
+    }
+
+    public static void topSort(ArrayList<Edge>[] graph) {
+        int[] indeg = new int[graph.length];
+        calcIndeg(graph, indeg);
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < indeg.length; i++) {
+            if (indeg[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            System.out.print(curr + " ");
+            for (int i = 0; i < graph[curr].size(); i++) {
+                Edge e = graph[curr].get(i);
+                indeg[e.dest]--;
+                if (indeg[e.dest] == 0) {
+                    q.add(e.dest);
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static void printAllPaths(ArrayList<Edge>[] graph, int src, int dest, String path, boolean[] vis) {
+        if (src == dest) {
+            System.out.println(path + dest);
+            return;
+        }
+        vis[src] = true;
+        for (int i = 0; i < graph[src].size(); i++) {
+            Edge e = graph[src].get(i);
+            if (!vis[e.dest]) {
+                printAllPaths(graph, e.dest, dest, path + src + " -> ", vis);
+            }
+        }
+        vis[src] = false;
+    }
+
+    static class Pair implements Comparable<Pair> {
+        int n;
+        int path;
+
+        public Pair(int n, int path) {
+            this.n = n;
+            this.path = path;
+        }
+
+        @Override
+        public int compareTo(Pair p2) {
+            return this.path - p2.path;
+        }
+    }
+
+    public static void dijkstra(ArrayList<Edge>[] graph, int src) {
+        int[] dist = new int[graph.length];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+        boolean[] vis = new boolean[graph.length];
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(src, 0));
+
+        while (!pq.isEmpty()) {
+            Pair curr = pq.remove();
+            if (!vis[curr.n]) {
+                vis[curr.n] = true;
+                for (int i = 0; i < graph[curr.n].size(); i++) {
+                    Edge e = graph[curr.n].get(i);
+                    int u = e.src;
+                    int v = e.dest;
+                    int wt = e.wt;
+
+                    if (dist[u] + wt < dist[v]) {
+                        dist[v] = dist[u] + wt;
+                        pq.add(new Pair(v, dist[v]));
+                    }
+                }
+            }
+        }
+
+        System.out.println("Shortest paths from source " + src + ":");
+        for (int i = 0; i < dist.length; i++) {
+            System.out.println("Vertex " + i + " -> " + dist[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int V = 7;
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
+
+        System.out.println("BFS traversal:");
+        bfs(graph);
+
+        System.out.println("DFS traversal:");
+        dfs(graph, 0, new boolean[V]);
+        System.out.println();
+
+        boolean[] visited = new boolean[V];
+        System.out.println("Has path from 0 to 5: " + hasPath(graph, 0, 5, visited));
+
+        System.out.println("Topological Sort:");
+        topSort(graph);
+
+        int src = 0, dest = 5;
+        System.out.println("All paths from " + src + " to " + dest + ":");
+        printAllPaths(graph, src, dest, "", new boolean[V]);
+
+        System.out.println("Dijkstra's shortest paths:");
+        dijkstra(graph, 0);
+    }
+}
