8450ce91476157e846a6c6fc4351c2efa695ef48
AMANPATEL <amanpatel8844382@gmail.com>
2024-08-03 08:27:25 +0530

    W3resources ex 149,150 Completed

---
+++ Course/w3resourceExercise/JavaBasic/BasicExcercisePart1/Checkifagivenstringisapermutationofanottherspecifiedstring_ex_149.java
@@ -0,0 +1,27 @@
+public class Checkifagivenstringisapermutationofanottherspecifiedstring_ex_149 {
+    public static void main(String[] args) {
+        String str1 = "xxyz";
+        String str2 = "yxzx";
+        System.out.println("Original Strings: " + str1 + " " + str2);
+        System.out.println(stringPermutation(str1, str2));
+
+    }
+
+    public static boolean stringPermutation(String str1, String str2) {
+        int[] arr = new int[500];
+
+        for (int i = 0; i < str1.length(); i++) {
+            arr[(int) str1.charAt(i)] += 1;
+        }
+
+        for (int i = 0; i < str2.length(); i++) {
+            arr[(int) str2.charAt(i)] -= 1;
+
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0)
+                return false;
+        }
+        return true;
+    }
+}
