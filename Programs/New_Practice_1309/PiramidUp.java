8b6a535f6bd59b91f34a390d2efd02f5498d3767
AMANPATEL <amanpatel8844382@gmail.com>
2024-09-14 21:31:44 +0530

    down reverse Piramid  full

--- Programs/New_Practice_1309/PiramidUp.java
+++ Programs/New_Practice_1309/PiramidUp.java
@@ -55,19 +55,24 @@ public class PiramidUp {
 
         }
 
-        // for (int i = 0; i <= n; i++) {
+        // for down full
 
-        // System.out.println();
+        for (int i = n; i >= 0; i--) {
+            // Print leading spaces
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
 
-        // }
+            for (int j = 0; j < (i * 2) - 1; j++) {
+                System.out.print("*");
+            }
 
-        // for (int i = 0; i <= n; i++) {
-        // for (int j = 0; j <= i; j++) {
-        // System.out.print("*");
-        // }
-        // System.out.println();
-        // }
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
 
     }
-
 }
