c6c1ef3e06cecbe8abe600b93efa02fb65dda231
AMANPATEL <amanpatel8844382@gmail.com>
2024-09-15 08:07:01 +0530

    remove other code

--- Programs/New_Practice_1309/PiramidUp.java
+++ Programs/New_Practice_1309/PiramidUp.java
@@ -75,20 +75,7 @@ public class PiramidUp {
         }
 
         // for left side full
-        for (int i = 1; i <= 5; i++) {
-
-            for (int j = 0; j <= n - i; j++) {
-                System.out.print("  ");
-            }
-
-            for (int j = 1; j <= 2 * i - 1; j++) {
-                System.out.print("*");
-            }
-
-            System.out.println();
-
-        }
-
+       
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
