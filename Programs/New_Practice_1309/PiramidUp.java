4f621aeb6c8ee47b691f6b5bdc20f26564f5275b
AMANPATEL <amanpatel8844382@gmail.com>
2024-09-15 08:05:37 +0530

    left side pattern completed

--- Programs/New_Practice_1309/PiramidUp.java
+++ Programs/New_Practice_1309/PiramidUp.java
@@ -74,5 +74,41 @@ public class PiramidUp {
             System.out.println();
         }
 
+        // for left side half
+
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = 0; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 4; i >= 1; i--) {
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
